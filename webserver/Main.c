7975fac69bbed4ff0cef0aa2c898bb4bf56b9981
Guillaume HOCHART <hochartg@hevea13.iut-infobio.priv.univ-lille1.fr>
2016-09-20 16:33:48 +0200

    main.c

--- webserver/Main.c
+++ webserver/Main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-int main ( int argc , char ** argv ){
+#include "socket.h"
+#include <unistd.h>
+#define BSIZE 1024
 
-  if (argc>1&&strcmp(argv[1],"-advice") == 0) {
-    printf("Don’t Panic!\n");
-    return 42;
+int main ( /*int argc , char ** argv*/ ){
+  int s=creer_serveur(8080);
+  int n=0;
+  char b[BSIZE];
+  while(1){
+    int a=accept(s,NULL,NULL);
+  
+    write(a,"Bienvenue sur notre serveur\n",28);
+  
+    while ((n=read(a,b,BSIZE))>0){
+      write(1,b,n);
+      write(a,b,n);
+      sleep(1);
+    }
   }
-  printf ("Need an advice?\n");
   return 0;
 }
