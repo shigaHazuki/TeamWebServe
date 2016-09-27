ad2ec5d625c69ee6be858a5542367c252514e192
Guillaume HOCHART <hochartg@frene04.iut-infobio.priv.univ-lille1.fr>
2016-09-27 14:07:45 +0200

    gestion signal

--- webserver/Main.c
+++ webserver/Main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "socket.h"
+#include "Main.h"
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 #define BSIZE 1024
 
 int main ( /*int argc , char ** argv*/ ){
@@ -10,14 +13,20 @@ int main ( /*int argc , char ** argv*/ ){
   char b[BSIZE];
   while(1){
     int a=accept(s,NULL,NULL);
-  
+    initialiser_signaux();
     write(a,"Bienvenue sur notre serveur\n",28);
   
     while ((n=read(a,b,BSIZE))>0){
+      
       write(1,b,n);
       write(a,b,n);
       sleep(1);
     }
   }
   return 0;
+}
+void initialiser_signaux ( void ){
+  if (signal(SIGPIPE, SIG_IGN) ==SIG_ERR){
+    perror("signal");
+  }
 }
